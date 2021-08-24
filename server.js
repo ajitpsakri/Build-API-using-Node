76afe1e7e0326c609ae9febf1e11eb9be6baf6e1
ajitpsakri <sakriajit@gmail.com>
2021-08-24 23:04:05 +0530

    basic routing

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ const express = require('express') //importing express package
 const app = express() // calling express 
 
 app.listen("3000") //listen to what ever client is saying @ localhost:3000
-app.use(express.static(/*root directory*/"public")) //use abilility of express file to host static files
+
+let urlResponse = []
+
+app.use(express.static("public"))
+
+app.get('/:colorName', function getParameters(req, res) {
+    res.send("color used is " + req.params.colorName)
+})
+
