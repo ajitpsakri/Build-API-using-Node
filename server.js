aff90356d677fd79a65c776ea5c3f0ec50d1ecb9
ajitpsakri <sakriajit@gmail.com>
2021-08-24 14:33:22 +0530

    HTML:connected to index.html

--- server.js
+++ server.js
@@ -2,3 +2,5 @@ const express = require('express') //importing express package
 
 const app = express() // calling express 
 
+app.listen("3000") //listen to what ever client is saying @ localhost:3000
+app.use(express.static(/*root directory*/"public")) //use abilility of express file to host static files
